a3144013563542d0d9e29f45bc56a1ea18e94027
Yannick Dossou <dossouyannick@gmail.com>
2026-02-21 19:56:51 +0100

    feat: initialize repo with Nuxt, Prismic and all my tooling

---
+++ nuxt.config.ts
@@ -0,0 +1,56 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// @ts-expect-error Don't know why this fires
+import { apiEndpoint, repositoryName } from "./slicemachine.config.json";
+import "./shared/lib/env";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: "Prismic + Nuxt Minimal Starter",
+      htmlAttrs: { lang: "en" },
+      meta: [{ charset: "utf-8" }],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+
+  css: ["./app/assets/css/main.css"],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: [
+    "motion-v/nuxt",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxtjs/prismic",
+    "@nuxtjs/seo",
+    "@nuxt/ui",
+  ],
+
+  // Module Configuration
+  prismic: {
+    endpoint: apiEndpoint || repositoryName,
+    preview: "/api/preview",
+    clientConfig: {
+      routes: [
+        { type: "page", uid: "home", path: "/" },
+        { type: "page", path: "/:uid" },
+      ],
+    },
+  },
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+
+  compatibilityDate: "2026-01-02",
+});
